e00dcc541f2a57087e32214742ad1ec4442498b2
Dilshan <dopatha@nc.rr.com>
2020-08-17 17:39:47 -0400

    updating

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.static("public"));
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("/routes/apiRoutes.js")(app);
+require("/routes/htmlRoutes.js")(app);
 
 // LISTENER
 // The below code effectively "starts" our server
