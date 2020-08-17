396141c46a8c0d259bdc0711a122a9ed69b14280
Dilshan <dopatha@nc.rr.com>
2020-08-17 18:23:46 -0400

    updating server.js&&package.json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,3 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 var tableData = require("../data/tableData");
 var waitListData = require("../data/waitinglistData");
 
--- server.js
+++ server.js
@@ -1,28 +1,20 @@
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
 var express = require("express");
+var fs = require("fs");
+const path = require('path');
 
-// Tells node that we are creating an "express" server
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname));
 
-// CSS
 app.use(express.static("public"));
 
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require("/routes/apiRoutes.js")(app);
-require("/routes/htmlRoutes.js")(app);
+require("/routes/apiRoutes")(app);
+require("/routes/htmlRoutes")(app);
 
-// LISTENER
-// The below code effectively "starts" our server
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
   });
